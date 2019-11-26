0519fcafc71fda308684c26258f1a93722a5bf1c
Bohdan <bohdan.kovalevych@nure.ua>
2019-11-26 15:12:01 +0200

    liqpay changed button

--- src/components/Profile/ProfilePage.js
+++ src/components/Profile/ProfilePage.js
@@ -5,34 +5,51 @@ import jwt_decode from 'jwt-decode'
 import {invitedLink} from '../../functions/UserFunctions.js'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import {getButtonToReplenish} from '../../functions/UserFunctions.js'
-
+import {Modal, Button} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 class ProfilePage extends Component {
   constructor() {
+    // const box = (<div className="replenish" >
+    //   <input className="inputSumm_Field" type='number' min='1' max='1000' name="amount" value={this.state.amount} onChange={this.onChange}/> UAH
+    //   <div className="plusBtn" onClick={this.getBtn}>+</div>
+    // </div>)
     super();
     this.handleShow = this.handleShow.bind(this);
     this.handleClose = this.handleClose.bind(this);
+    this.setReplenishBoxHidden = this.setReplenishBoxHidden.bind(this);
+    this.setReplenishBoxShow = this.setReplenishBoxShow.bind(this);
     this.onChange = this.onChange.bind(this);
     this.getBtn = this.getBtn.bind(this);
     this.state = {
       show: false,
+      showModal: false,
       amount: 1,
       _id: 0,
       nickname: '',
       email: '',
       register_date: '',
-      inviteLink: ''
+      inviteLink: '',
+      isReplenishOpen:false
     };
   }
 
   getBtn() {
+    let doc = document.getElementById('btn_to_pay');
+    // doc.disabled = true;
     getButtonToReplenish(this.state.nickname, this.state.amount).then(result => {
       let language = 'en';
-      document.getElementById('btn_to_pay').innerHTML =
-          '<form method="POST" action="https://www.liqpay.ua/api/3/checkout" accept-charset="utf-8">' +
+      let data = result.data;
+      let signature = result.signature;
+
+      doc.parentNode.innerHTML +=
+          '<form id="formReplenish" method="POST" action="https://www.liqpay.ua/api/3/checkout" accept-charset="utf-8">' +
           '<input type="hidden" name="data" value="'+result.data+'" />' +
           '<input type="hidden" name="signature" value="'+result.signature+'" />' +
-          '<input type="image" src="//static.liqpay.ua/buttons/p1'+language+'.radius.png" name="btn_text" />' +
-          '</form>'
+          '<input type="button"  name="btn_text" />' +
+          '</form>';
+      let formReplenish = document.getElementById("formReplenish");
+      formReplenish.submit();
+      // doc.disabled = false;
     }).catch(err => {
       console.log(err)
     })
@@ -42,11 +59,12 @@ class ProfilePage extends Component {
     const name = e.target.name;
     const value = e.target.value;
     this.setState({[name]: value})
+
   }
 
   componentDidMount(){
-    const token = localStorage.usertoken
-    const decoded = jwt_decode(token)
+    const token = localStorage.usertoken;
+    const decoded = jwt_decode(token);
     const date = new Date(decoded.register_date).getDate() + '/' + (new Date(decoded.register_date).getMonth() + 1) + '/' + new Date(decoded.register_date).getFullYear();
     var invLink = invitedLink(decoded.email);
       this.setState({
@@ -59,6 +77,15 @@ class ProfilePage extends Component {
   }
 
 
+    setReplenishBoxHidden() {
+       this.setState({showModal: false});
+    }
+
+    setReplenishBoxShow() {
+      this.setState({showModal: true});
+      this.getBtn()
+    }
+
     handleClose() {
       this.setState({ show: false });
     }
@@ -68,18 +95,37 @@ class ProfilePage extends Component {
     }
   // start preparations mining processing selling finish
 render(){
+
   return (
+
     <div className="ProfilePage">
       <div className="myProfile">
         <div className="row">
         <div className="name_balance">
         <h2>@{this.state.nickname} <span className="id">id: {this.state._id}</span></h2>
-        <div className="balance">balance: <div className="summ">$0</div>
-          <input type='number' min='1' max='1000' name="amount" value={this.state.amount} onChange={this.onChange}/> UAH
-          <div className="plusBtn" onClick={this.getBtn}>+</div>
-          <div id='btn_to_pay'>
+        <div className="balance">
+          <div className="balanceRow"> balance: <div className="summ">$0</div></div>
+          <button onClick={this.setReplenishBoxShow}>Replenish</button>
+
+          <Modal show={this.state.showModal} onHide={this.setReplenishBoxHidden}>
+            <Modal.Header closeButton>
+              <Modal.Title>Replenish yor balance</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <div >
+                <input className="inputSumm_Field" type='number' min='1' max='1000' name="amount" value={this.state.amount} onChange={this.onChange}/> UAH
+                <button id='btn_to_pay' onClick={this.getBtn}>Pay</button>
+              </div>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={this.setReplenishBoxHidden}>Close</Button>
+
+            </Modal.Footer>
+          </Modal>
+          <div id='replenishBox' >
 
           </div>
+
         </div>
         </div>
         <Link to="/settings"><div className="settings">settings</div></Link>
