fc81442b8b2441b13cccdccbf8489be2bc67dea5
Bohdan <32486313+bkovalevych@users.noreply.github.com>
2019-12-15 18:19:00 +0200

    What is going on?

--- src/components/Profile/ProfilePage.js
+++ src/components/Profile/ProfilePage.js
@@ -4,9 +4,9 @@ import {Component} from 'react'
 import jwt_decode from 'jwt-decode'
 import {invitedLink} from '../../functions/UserFunctions.js'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import {getButtonToReplenish, getWithdrawData, getBalance} from '../../functions/UserFunctions.js'
+import {getButtonToReplenish, getWithdrawData, getBalance, getTransactions} from '../../functions/UserFunctions.js'
 import {Modal, Button} from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 class ProfilePage extends Component {
   constructor() {
@@ -15,6 +15,7 @@ class ProfilePage extends Component {
     //   <div className="plusBtn" onClick={this.getBtn}>+</div>
     // </div>)
     super();
+    this.getTransactions = getTransactions.bind(this);
     this.handleShow = this.handleShow.bind(this);
     this.handleClose = this.handleClose.bind(this);
     this.setReplenishBoxHidden = this.setReplenishBoxHidden.bind(this);
@@ -39,9 +40,12 @@ class ProfilePage extends Component {
       register_date: '',
       inviteLink: '',
       isReplenishOpen:false,
-      balance: 0
+      balance: 0,
+      data : null
     };
   }
+
+
 
   sendTransactionToWithdraw(e) {
     e.preventDefault();
@@ -72,12 +76,12 @@ class ProfilePage extends Component {
         this.state.secondName,
         this.state.card,
         this.state.amountWithdraw).then( result => {
-          if (result.errors) {
-            console.log(result.errors);
-          }
-          console.log(result);
-          alert(result.data);
-        }).catch(err => {alert(err)})
+      if (result.errors) {
+        console.log(result.errors);
+      }
+      console.log(result);
+      alert(result.data);
+    }).catch(err => {alert(err)})
 
 
 
@@ -119,137 +123,177 @@ class ProfilePage extends Component {
     this.getBalance(decoded._id).then(data => {
       console.log("componentDidMount " + data);
       this.setState({
-         balance : data.data
-       })
-     });
-      this.setState({
-        _id: decoded._id,
-        nickname: decoded.nickname,
-        email: decoded.email,
-        register_date: date,
-        inviteLink: invLink,
-        balance: decoded.balance.toString()
+        balance : data.data
       })
+    });
+    this.getTransactions(decoded._id).then(resp => {
+      resp.data.forEach(elem => {
+        let st = new Date(elem.startDate);
+        let fn = new Date(elem.endDate);
+        elem.startDate = `${st.getDate()}/${st.getMonth()}`;
+        elem.endDate = `${fn.getDate()}/${fn.getMonth()}`;
+        elem.amount = elem.amount.$numberDecimal;
+        elem.plan.profit = elem.plan.profit.$numberDecimal;
+      });
+      this.setState({data: resp.data});
+    }).catch(err => {
+      console.log(err.toString());
+    });
+    this.setState({
+      _id: decoded._id,
+      nickname: decoded.nickname,
+      email: decoded.email,
+      register_date: date,
+      inviteLink: invLink,
+      balance: decoded.balance.toString()
+    })
 
   }
-    setWithdrawBoxHidden() {
-      this.setState({showModalWithDraw: false});
-    }
+  setWithdrawBoxHidden() {
+    this.setState({showModalWithDraw: false});
+  }
 
-    setWithdrawBoxShow() {
-      this.setState({showModalWithDraw: true});
-    }
+  setWithdrawBoxShow() {
+    this.setState({showModalWithDraw: true});
+  }
 
-    setReplenishBoxHidden() {
-       this.setState({showModal: false});
-    }
+  setReplenishBoxHidden() {
+    this.setState({showModal: false});
+  }
 
-    setReplenishBoxShow() {
-      this.setState({showModal: true});
-    }
+  setReplenishBoxShow() {
+    this.setState({showModal: true});
+  }
 
-    handleClose() {
-      this.setState({ show: false });
-    }
+  handleClose() {
+    this.setState({ show: false });
+  }
 
-    handleShow() {
-      this.setState({ show: true });
-    }
+  handleShow() {
+    this.setState({ show: true });
+  }
   // start preparations mining processing selling finish
-render(){
+
+  renderTable = () => {
+    return this.state.data? this.state.data.map(transaction =>
+        (
+            <tr key={transaction._id}>
+              <td>{transaction.plan.name}</td>
+              <td>{transaction.startDate}</td>
+              <td>{transaction.endDate}</td>
+              <td>{transaction.amount}</td>
+              <td>{transaction.plan.profit}</td>
+            </tr>
+        )
+    ): "";
+  };
+
+  render(){
 
     console.log(this.state.balance);
-  return (
-    <div className="ProfilePage">
-      <div className="myProfile">
-        <div className="row">
-        <div className="name_balance">
-        <h2>@{this.state.nickname} <span className="id">id: {this.state._id}</span></h2>
-        <div className="balance">
-          <div className="balanceRow"> balance: <div className="summ">{this.state.balance.toString()} uah</div></div>
-          <button onClick={this.setReplenishBoxShow}>Replenish</button>
-          <button onClick={this.setWithdrawBoxShow}>Withdraw money to card</button>
-          <Modal show={this.state.showModalWithDraw} onHide={this.setWithdrawBoxHidden}>
-            <Modal.Header closeButton>
-              <Modal.Title>Withdraw your money</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <form className='withdrawBox' onSubmit={this.sendTransactionToWithdraw}>
-                  <p>Card</p> <input type='text' value={this.state.card} name='card' onChange={this.onChange} pattern="^(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))$"/>
-                  <p>Amount</p> <input className="inputSumm_Field" type='number' min='1' max='10000' name="amountWithdraw" value={this.state.amountWithdraw} onChange={this.onChange}/> UAH
-                  <button id='btn_to_withdraw' type='submit'>send</button>
-              </form>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={this.setWithdrawBoxHidden}>Close</Button>
+    return (
+        <div className="ProfilePage">
+          <div className="myProfile">
+            <div className="row">
+              <div className="name_balance">
+                <h2>@{this.state.nickname} <span className="id">id: {this.state._id}</span></h2>
+                <div className="balance">
+                  <div className="balanceRow"> balance: <div className="summ">{this.state.balance.toString()} uah</div></div>
+                  <button onClick={this.setReplenishBoxShow}>Replenish</button>
+                  <button onClick={this.setWithdrawBoxShow}>Withdraw money to card</button>
+                  <Modal show={this.state.showModalWithDraw} onHide={this.setWithdrawBoxHidden}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>Withdraw your money</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      <form className='withdrawBox' onSubmit={this.sendTransactionToWithdraw}>
+                        <p>Card</p> <input type='text' value={this.state.card} name='card' onChange={this.onChange} pattern="^(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))$"/>
+                        <p>Amount</p> <input className="inputSumm_Field" type='number' min='1' max='10000' name="amountWithdraw" value={this.state.amountWithdraw} onChange={this.onChange}/> UAH
+                        <button id='btn_to_withdraw' type='submit'>send</button>
+                      </form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="secondary" onClick={this.setWithdrawBoxHidden}>Close</Button>
 
-            </Modal.Footer>
-          </Modal>
-          <Modal show={this.state.showModal} onHide={this.setReplenishBoxHidden}>
-            <Modal.Header closeButton>
-              <Modal.Title>Replenish yor balance</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <div >
-                <input className="inputSumm_Field" type='number' min='1' max='1000' name="amount" value={this.state.amount} onChange={this.onChange}/> UAH
-                <button id='btn_to_pay' onClick={this.getBtn}>Pay</button>
-              </div>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={this.setReplenishBoxHidden}>Close</Button>
+                    </Modal.Footer>
+                  </Modal>
+                  <Modal show={this.state.showModal} onHide={this.setReplenishBoxHidden}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>Replenish yor balance</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      <div >
+                        <input className="inputSumm_Field" type='number' min='1' max='1000' name="amount" value={this.state.amount} onChange={this.onChange}/> UAH
+                        <button id='btn_to_pay' onClick={this.getBtn}>Pay</button>
+                      </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="secondary" onClick={this.setReplenishBoxHidden}>Close</Button>
 
-            </Modal.Footer>
-          </Modal>
-          <div id='replenishBox' >
+                    </Modal.Footer>
+                  </Modal>
+                  <div id='replenishBox' >
 
-          </div>
+                  </div>
 
-        </div>
-        </div>
-        <Link to="/settings"><div className="settings">settings</div></Link>
-        </div>
-        <div className="profileData">
-          <div className="myProfileData">
-            <div className="col">
-              <div className="profile_info">
-                <div className="profile_info_header">profile info </div>
-                <div>registration date: {this.state.register_date}</div>
-                <div>email: {this.state.email}</div>
                 </div>
               </div>
-              <span className="vertical_divider"></span>
-              <div className="col">
-              <div className="profile_statistics">
-                <div className="profile_info_header">statistics</div>
-                <div className="statistics_row">
+              <Link to="/settings"><div className="settings">settings</div></Link>
+            </div>
+            <div className="profileData">
+              <div className="myProfileData">
                 <div className="col">
-                <div>invested:</div>
-                <div className="invested">$0</div>
+                  <div className="profile_info">
+                    <div className="profile_info_header">profile info </div>
+                    <div>registration date: {this.state.register_date}</div>
+                    <div>email: {this.state.email}</div>
+                  </div>
                 </div>
+                <span className="vertical_divider"></span>
                 <div className="col">
-                <div>earned:</div>
-                <div className="earned">$0</div>
-                </div>
-                </div>
+                  <div className="profile_statistics">
+                    <div className="profile_info_header">statistics</div>
+                    <div className="statistics_row">
+                      <div className="col">
+                        <div>invested:</div>
+                        <div className="invested">$0</div>
+                      </div>
+                      <div className="col">
+                        <div>earned:</div>
+                        <div className="earned">$0</div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
+            </div>
+          </div>
+
+          <div className="myProjectsBox">
+            <h2>My projects</h2>
+            <div className="myProjects">
+              <div className="addProject">
+                <div className="plusBtn">+</div>
+                <Link to='/' className="plusText" onClick={this.handleShow}>Add project</Link>
+              </div>
+              <div className="transactionBlock">
+                <table id="customers">
+                  <tr>
+                    <th>Plan</th>
+                    <th>Start Date</th>
+                    <th>End Date</th>
+                    <th>Invested</th>
+                    <th>Profit</th>
+                  </tr>
+                  {this.renderTable()}
+                </table>
+              </div>
+            </div>
+
           </div>
         </div>
-      </div>
-
-      <div className="myProjectsBox">
-        <h2>My projects</h2>
-        <div className="myProjects">
-          <div className="addProject">
-            <div className="plusBtn">+</div>
-            <div className="plusText" onClick={this.handleShow}>Add project</div>
-          </div>
-        </div>
-
-      </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default ProfilePage;
